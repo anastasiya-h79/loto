aad036fca98dd1a199d0e283a39cb04b57a4df58
anastasiya-h79 <anastasiya-h79@yandex.ru>
2019-12-15 17:05:20 +0300

    function

---
+++ loto_functions.py
@@ -0,0 +1,11 @@
+import loto_classes
+import random
+
+def choice_of_gamers(num_of_gamer):
+    player = None
+    while player not in ['c', 'h']:
+        print(num_of_gamer, ' игроком будет человек или компьютер?')
+        player = input('(c/h): ')
+    if player == 'c':
+        return loto_classes.Comp()
+    return loto_classes.Gamer()
